b99445df735b059aa3578edb0ed091e181da329d
Dariusz Rajtak (DRA) <DRA@kmd.dk>
2018-08-10 14:15:44 +0200

    Initial OSS commit

---
+++ CoreTeamPrivacyTool/FormAAT.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace CoreTeamPrivacyTool
+{
+    partial class FormAAT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAAT));
+            this.textBoxSecret = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDecrypted = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypted = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonPaste = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.buttonCopyE = new System.Windows.Forms.Button();
+            this.buttonPasteE = new System.Windows.Forms.Button();
+            this.buttonClearE = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxSecret
+            // 
+            this.textBoxSecret.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSecret.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSecret.Location = new System.Drawing.Point(12, 51);
+            this.textBoxSecret.Name = "textBoxSecret";
+            this.textBoxSecret.PasswordChar = '☺';
+            this.textBoxSecret.Size = new System.Drawing.Size(562, 31);
+            this.textBoxSecret.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Secret Key:";
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Decrypted:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxDecrypted
+            // 
+            this.textBoxDecrypted.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxDecrypted.Location = new System.Drawing.Point(3, 32);
+            this.textBoxDecrypted.Multiline = true;
+            this.textBoxDecrypted.Name = "textBoxDecrypted";
+            this.textBoxDecrypted.Size = new System.Drawing.Size(273, 278);
+            this.textBoxDecrypted.TabIndex = 3;
+            // 
+            // textBoxEncrypted
+            // 
+            this.textBoxEncrypted.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxEncrypted.Location = new System.Drawing.Point(282, 32);
+            this.textBoxEncrypted.Multiline = true;
+            this.textBoxEncrypted.Name = "textBoxEncrypted";
+            this.textBoxEncrypted.Size = new System.Drawing.Size(274, 278);
+            this.textBoxEncrypted.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(282, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(274, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Encrypted:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxEncrypted, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxDecrypted, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 92);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.395973F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.60403F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 58F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(559, 372);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.buttonEncrypt);
+            this.flowLayoutPanel1.Controls.Add(this.buttonClear);
+            this.flowLayoutPanel1.Controls.Add(this.buttonPaste);
+            this.flowLayoutPanel1.Controls.Add(this.buttonCopy);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 313);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(279, 59);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(172, 3);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(104, 53);
+            this.buttonEncrypt.TabIndex = 7;
+            this.buttonEncrypt.Text = "Encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonClear.Location = new System.Drawing.Point(91, 3);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 53);
+            this.buttonClear.TabIndex = 8;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonPaste
+            // 
+            this.buttonPaste.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPaste.Location = new System.Drawing.Point(10, 3);
+            this.buttonPaste.Name = "buttonPaste";
+            this.buttonPaste.Size = new System.Drawing.Size(75, 53);
+            this.buttonPaste.TabIndex = 9;
+            this.buttonPaste.Text = "Paste";
+            this.buttonPaste.UseVisualStyleBackColor = true;
+            this.buttonPaste.Click += new System.EventHandler(this.buttonPaste_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCopy.Location = new System.Drawing.Point(201, 62);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(75, 0);
+            this.buttonCopy.TabIndex = 10;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.buttonDecrypt);
+            this.flowLayoutPanel2.Controls.Add(this.buttonCopyE);
+            this.flowLayoutPanel2.Controls.Add(this.buttonPasteE);
+            this.flowLayoutPanel2.Controls.Add(this.buttonClearE);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(279, 313);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(280, 59);
+            this.flowLayoutPanel2.TabIndex = 9;
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Location = new System.Drawing.Point(3, 3);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(104, 53);
+            this.buttonDecrypt.TabIndex = 8;
+            this.buttonDecrypt.Text = "Decrypt";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // buttonCopyE
+            // 
+            this.buttonCopyE.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCopyE.Location = new System.Drawing.Point(113, 3);
+            this.buttonCopyE.Name = "buttonCopyE";
+            this.buttonCopyE.Size = new System.Drawing.Size(75, 53);
+            this.buttonCopyE.TabIndex = 11;
+            this.buttonCopyE.Text = "Copy";
+            this.buttonCopyE.UseVisualStyleBackColor = true;
+            this.buttonCopyE.Click += new System.EventHandler(this.buttonCopyE_Click);
+            // 
+            // buttonPasteE
+            // 
+            this.buttonPasteE.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPasteE.Location = new System.Drawing.Point(194, 3);
+            this.buttonPasteE.Name = "buttonPasteE";
+            this.buttonPasteE.Size = new System.Drawing.Size(75, 53);
+            this.buttonPasteE.TabIndex = 12;
+            this.buttonPasteE.Text = "Paste";
+            this.buttonPasteE.UseVisualStyleBackColor = true;
+            this.buttonPasteE.Click += new System.EventHandler(this.buttonPasteE_Click);
+            // 
+            // buttonClearE
+            // 
+            this.buttonClearE.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonClearE.Location = new System.Drawing.Point(3, 62);
+            this.buttonClearE.Name = "buttonClearE";
+            this.buttonClearE.Size = new System.Drawing.Size(75, 0);
+            this.buttonClearE.TabIndex = 13;
+            this.buttonClearE.Text = "Clear";
+            this.buttonClearE.UseVisualStyleBackColor = true;
+            this.buttonClearE.Click += new System.EventHandler(this.buttonClearE_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(586, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolToolStripMenuItem
+            // 
+            this.toolToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.toolToolStripMenuItem.Name = "toolToolStripMenuItem";
+            this.toolToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.toolToolStripMenuItem.Text = "Tool";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // FormAAT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 476);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSecret);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(602, 476);
+            this.Name = "FormAAT";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AAT: Core Team\'s Secrecy Tool";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSecret;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDecrypted;
+        private System.Windows.Forms.TextBox textBoxEncrypted;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonPaste;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Button buttonCopyE;
+        private System.Windows.Forms.Button buttonPasteE;
+        private System.Windows.Forms.Button buttonClearE;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ CoreTeamPrivacyTool/FormAAT.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoreTeamPrivacyTool
+{
+    public partial class FormAAT : Form
+    {
+        private static string Version = "0.2";
+
+        public FormAAT()
+        {
+            InitializeComponent();
+
+            this.Text += $" ({Version})";
+        }
+
+        internal const string Inputkey = "{EC40FDD0-115F-441F-8960-1BF3C7F848D5}";
+
+        private static RijndaelManaged NewRijndaelManaged(string passphrase)
+        {
+            if (passphrase == null)
+                throw new PassphraseToShortException();
+
+            var passphraseBytes = Encoding.ASCII.GetBytes(passphrase);
+            var key = new Rfc2898DeriveBytes(Inputkey, passphraseBytes);
+
+            var aesAlg = new RijndaelManaged();
+            aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+            aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+
+            return aesAlg;
+        }
+
+        public static string EncryptRijndael(string text, string passphrase)
+        {
+            if (string.IsNullOrEmpty(text))
+                return "";
+
+            var aesAlg = NewRijndaelManaged(passphrase);
+
+            var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+            var msEncrypt = new MemoryStream();
+            using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+            using (var swEncrypt = new StreamWriter(csEncrypt))
+            {
+                swEncrypt.Write(text);
+            }
+
+            return Convert.ToBase64String(msEncrypt.ToArray());
+        }
+
+        public static string DecryptRijndael(string cipherText, string passphrase)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                return "";
+
+            string text;
+
+            var aesAlg = NewRijndaelManaged(passphrase);
+            var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+            var cipher = Convert.FromBase64String(cipherText);
+
+            using (var msDecrypt = new MemoryStream(cipher))
+            {
+                using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (var srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        text = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            return text;
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBoxEncrypted.Text = EncryptRijndael(textBoxDecrypted.Text, textBoxSecret.Text);
+                textBoxDecrypted.Text = "";
+            }
+            catch (PassphraseToShortException)
+            {
+                MessageBox.Show(this, "Secret needs to be 8 chars at least", "Whoopsie", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Whoopsie", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBoxDecrypted.Text = DecryptRijndael(textBoxEncrypted.Text, textBoxSecret.Text);
+                textBoxEncrypted.Text = "";
+            }
+            catch (PassphraseToShortException)
+            {
+                MessageBox.Show(this, "Secret needs to be 8 chars at least", "Whoopsie", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Whoopsie", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonClearE_Click(object sender, EventArgs e)
+        {
+            textBoxEncrypted.Text = "";
+        }
+
+        private void buttonCopyE_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBoxEncrypted.Text, TextDataFormat.UnicodeText);
+        }
+
+        private void buttonPasteE_Click(object sender, EventArgs e)
+        {
+            textBoxEncrypted.Text = Clipboard.GetText(TextDataFormat.UnicodeText);
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBoxDecrypted.Text, TextDataFormat.UnicodeText);
+        }
+
+        private void buttonPaste_Click(object sender, EventArgs e)
+        {
+            textBoxDecrypted.Text = Clipboard.GetText(TextDataFormat.UnicodeText);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxDecrypted.Text = "";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var form = new FormAbout()
+            {
+                ShowInTaskbar = false,
+                StartPosition = FormStartPosition.CenterParent
+            })
+            {
+                form.ShowDialog(this);
+            }
+        }
+    }
+}
